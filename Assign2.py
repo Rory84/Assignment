82267e8a1f84f91571b7a3e6ea56a3c0c1a927f5
Rory84 <OLoughlin-R@ulster.ac.uk>
2022-05-22 15:58:54 +0100

    Plot map
    Load and display all datasets

--- Assign2.py
+++ Assign2.py
@@ -21,29 +21,38 @@ from rasterio.crs import CRS
 
 
 #Read sampling points table and convert to pandas dataframe
+'''line 1: read csv to pandas daataframe object named df,
+   line 2: select columns from CSV to import
+   line 3: print df object'''
 df = pd.read_csv("Data\Sampling_Points_coor.csv")
 df = df[["FID", "Lat_m", "Lon_m"]]
-'''select columns from CSV to import'''
 print(df)
 
 #Convert pandas DataFrame to geopandas GeoDataFrame
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Lon_m, df.Lat_m, crs=32630))
 '''Create Geoseries of point geometries from "Lat_m and "Long_m" columns,
- define coordinate reference system as WGS84 UTM Zone 30N using EPSG code,
- define Geodataframe as gdf'''
+define coordinate reference system as WGS84 UTM Zone 30N using EPSG code,
+define Geodataframe as gdf'''
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Lon_m, df.Lat_m, crs=32630))
 print(gdf)
 
 #Write GeoDataFrame to shapefile
-gdf.to_file("Data\Sampling_Points.shp", encoding="utf-8")
 '''Write GeoDataFrame to shapefile with geopandas .to_file method,
 define encoding method'''
+gdf.to_file("Data\Sampling_Points.shp", encoding="utf-8")
 
 #Load habitat map polygon shapefile
+'''line 1:Read Ards_Map.shp file to object named Habitat_map,
+   line 2:print Habitat_map object
+   line 3:define coordinate reference system of Habitat_map object'''
 Habitat_map = gpd.read_file("Data\Ards_Map.shp")
+Habitat_map = Habitat_map.to_crs(32630)
 print(Habitat_map)
+
+#Load Sampling_Points shapefile
+'''line 1: Load Sampling_Points shapefile to object named Points,
+   line 2:print Points object'''
 Points = gpd.read_file("Data\Sampling_Points.shp")
 print(Points)
-Habitat_map = Habitat_map.to_crs(32630)
 
 #Read raster chart file
 chart = rasterio.open("Data\Ards.tif")
@@ -52,8 +61,8 @@ chart = rasterio.open("Data\Ards.tif")
 #Plot all layers together
 fig, ax = plt.subplots()
 base = show(chart, ax=ax)
-Habitat_map.plot(ax=ax, column="Habitat", cmap='Pastel2', legend=True);
-Points.plot(ax=ax, color="red", legend=True);
+Habitat_map.plot(ax=ax, column="Habitat", cmap='Pastel2');
+Points.plot(ax=ax, color="red");
 plt.show()
 
 
